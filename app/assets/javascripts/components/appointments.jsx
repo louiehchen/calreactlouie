60f28b7194a3b7c5050e07f42fd888b39c3b060a
Louie <louiehchen@gmail.com>
2017-12-30 13:55:46 -0800

    working app

--- app/assets/javascripts/components/appointments.jsx
+++ app/assets/javascripts/components/appointments.jsx
@@ -17,7 +17,7 @@ var Appointments = React.createClass({
 						{appointment: appointment})
 					.done(function(data) {
 						this.addNewAppointment(data);
-					}).bind(this);
+					}.bind(this));
 	},
 
 	addNewAppointment: function(appointment) {
@@ -32,8 +32,8 @@ var Appointments = React.createClass({
 		return (
 			<div>
 				<AppointmentForm
-					input_title={this.state.title}
-					input_appt_time={this.state.appt_time}
+					title={this.state.title}
+					appt_time={this.state.appt_time}
 					onUserInput={this.handleUserInput}
 					onFormSubmit={this.handleFormSubmit} />
 				<AppointmentsList appointments={this.state.appointments} />
